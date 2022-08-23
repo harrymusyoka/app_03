3d8994ec0a34a928b14b3909eaf326c247995e66
harrymusyoka <musyokaoracle@gmail.com>
2022-08-23 13:28:54 -0400

    updates-3

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 
+gunicorn -b 127.0.0.1:8001 app_03.wsgi:application
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_03.settings')
