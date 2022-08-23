45568ea78a271faee98cdfaa6d52679d0cf89617
harrymusyoka <musyokaoracle@gmail.com>
2022-08-23 13:11:27 -0400

    updates-2

---
+++ apps03/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.contrib import messages
+
+
+
+def login(request):
+      HttpResponse('apps03/index.html')
