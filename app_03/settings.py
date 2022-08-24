990ac98cc3affef85fd831850dbf29ee71f388d9
harrymusyoka <musyokaoracle@gmail.com>
2022-08-24 01:26:44 -0400

    updates-4

--- app_03/settings.py
+++ app_03/settings.py
@@ -15,11 +15,10 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
-
-
-
-
 from django.core.management.utils import get_random_secret_key
+
+
+
 
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
@@ -97,7 +96,20 @@ WSGI_APPLICATION = 'app_03.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+[label django_app/settings.py]
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
