360988519fe13c210de03cf61fc41a0286327db8
harrymusyoka <musyokaoracle@gmail.com>
2022-08-26 12:26:26 -0400

    updates-4

--- apps03/views.py
+++ apps03/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib import messages
-
+from .forms import rentalunitForm
 
 def   login(request):
       a = "Hello"
