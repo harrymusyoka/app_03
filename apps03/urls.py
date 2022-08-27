9228eeb4a4d1e9d2a1fdfbed7ac60eed8c00a3c1
harrymusyoka <musyokaoracle@gmail.com>
2022-08-27 13:21:40 -0400

    updates-4

--- apps03/urls.py
+++ apps03/urls.py
@@ -9,6 +9,27 @@ urlpatterns = [
 
     path('apps03/login/', views.login, name='login'),
     path('apps03/add_accomcat/', views.add_accomcat, name='add_accomcat'),
+    
+    
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('rentalunitpr/', views.rentalunitpr, name='rentalunitpr'),
+    path('apps03/dashboard/', views.dashboard, name='dashboard'),
+    path('apps03/polls_list/', views.polls_list, name='polls_list'),
+    path('apps03/task_create/', views.task_create, name='task_create'),
+    path('viewpro/', views.viewpro, name='viewpro'),
+
+
+
+    path('commcatsv/', views.commcatsv, name='commcatsv'), 
+
+    path('apps03/add_roomcat/', views.add_roomcat, name='add_roomcat'),
+    path('roomcatsv/', views.roomcatsv, name='roomcatsv'), 
+
+
+    path('apaccom0/add_rentalunit/', views.add_rentalunit, name='add_rentalunit'),
+    path('aapps03/add_guest/',views.add_guest, name='add_guest'),
+    path('addguest/',views.addguest, name='addguest'),
 
        ]
 
--- apps03/views.py
+++ apps03/views.py
@@ -36,3 +36,71 @@ def add_accomcat(request):
        template = loader.get_template('apps03/main-menu.html')
        return HttpResponse(template.render())
 
+
+
+def polls_list(request):
+      HttpResponse('apps03/poll_list.html')
+
+
+
+def dashboard(request):
+      template = loader.get_template('apps03/main-menu.html')
+      return HttpResponse(template.render())
+
+
+def task_create(request):
+      template = loader.get_template('apps03/main-menu.html')
+      return HttpResponse(template.render())
+
+def add_guest(request):
+      template = loader.get_template('apps03/main-menu.html')
+      return HttpResponse(template.render())
+
+def add_roomcat(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+
+def add_accomcat(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+def roomcatsv(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+
+def commcatsv(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+
+def add_rentalunit(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+
+
+def rentalunitpr(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+def addguest(request):
+       template = loader.get_template('apps03/main-menu.html')
+       return HttpResponse(template.render())
+
+def viewpro(request):
+     template = loader.get_template('apps03/main-menu.html')
+     return HttpResponse(template.render())
+
+
+
+
+
+
+
+
+
+
+
+
