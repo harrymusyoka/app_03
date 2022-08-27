ae2b48c52ad0de8e0a77ba9416e8d8521b798a7a
harrymusyoka <musyokaoracle@gmail.com>
2022-08-27 16:46:04 -0400

    updates-4

--- apps03/views.py
+++ apps03/views.py
@@ -12,11 +12,6 @@ from django.views.generic import CreateView
 
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=130)
-    email = models.EmailField(blank=True)
-    job_title = models.CharField(max_length=30, blank=True)
-    bio = models.TextField(blank=True)
 
 
 
