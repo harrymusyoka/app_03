d9cef081024fd7c1a93e7ce30fdfa343f99a5018
harrymusyoka <musyokaoracle@gmail.com>
2022-08-29 08:50:50 -0400

    updates-4

--- apps03/views.py
+++ apps03/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib import messages
-from django.db import models
-from django.views.generic import UpdateView
-from django.views.generic import CreateView
 
 
 
