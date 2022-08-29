686f741ee4f627ebe7d8c345419aa3daaca58fdd
harrymusyoka <musyokaoracle@gmail.com>
2022-08-29 09:45:36 -0400

    updates-4

--- apps03/views.py
+++ apps03/views.py
@@ -17,18 +17,9 @@ def   login(request):
       template = loader.get_template('apps03/main-menu.html')
       return HttpResponse(template.render())
       
-      
-def login1(request):
-   
-    if request.method == 'POST':
-        form = rentalunitForm(request.POST)
-        if form.is_valid():
-            print('valid')
-    else:
-        form = rentalunitForm()
-    return render(request,  'apps03/main-menu.html', {'form': form})
-    
-    
+def polls_list(request):
+      HttpResponse('apps03/poll_list.html')
+
  
 def dashboard(request):
       template = loader.get_template('apps03/main-menu.html')
