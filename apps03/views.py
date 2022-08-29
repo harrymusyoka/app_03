aa9318a027a40b95f6bb09ca824095771e497398
harrymusyoka <musyokaoracle@gmail.com>
2022-08-29 08:10:19 -0400

    updates-4

--- apps03/views.py
+++ apps03/views.py
@@ -35,12 +35,48 @@ def login1(request):
     return render(request,  'apps03/main-menu.html', {'form': form})
     
     
-    
+ #polls_list   
 
   
 
-def polls_list(request):
-      HttpResponse('apps03/poll_list.html')
+def add_rentalunit(request):
+      print()
+      print()
+
+      a = True
+
+      while a == True:
+
+       number1 = int(input("enter first number:"))
+       number2 = int(input("enter second number:"))
+       number3 = int(input("enter third number:"))
+       sum = number1 + number2 + number3
+
+       print()
+       print("\t\t======================================")
+       print()
+
+       print("Addition of three numbers is", " :-- ", sum)
+
+       print()
+       print("\t\t======================================")
+       print()
+
+      d = input("Do tou want to do it again ??   Y / N -- ").lower()
+
+      if d == "y":
+
+         print()
+         print("\t\t======================================")
+         print()
+
+         continue
+
+      else:
+
+        exit()
+   
+      HttpResponse('apps03/main-menu.html')
 
 
 
