e435313fea599680a5c492a8d8d45ec2c1f4ba5a
harrymusyoka <musyokaoracle@gmail.com>
2022-09-08 22:15:50 +0300

    upd01

--- apps03/views.py
+++ apps03/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 
 from .models import Article
 
+
+def   login(request):
+
+
+      #mydb = mysql.connector.connect(
+      #host="db-mysql-nyc3-16778-do-user-11647348-0.b.db.ondigitalocean.com",
+      #user="doadmin",
+      #password="AVNS_opSyNyIDAt49SbpzqG_",
+      #database="defaultdb")
+
+      #mycursor = mydb.cursor()
+
+      #sql = "DROP TABLE rentalunit"
+
+      #mycursor.execute(sql) 
+      a = "Hello"
+      print(a)
+
+      
+      template = loader.get_template('apps03/login.html')
+      return HttpResponse(template.render())
+
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {'year': year, 'article_list': a_list}
-    return render(request, 'news/year_archive.html', context)
+    return render(request, 'apps03/year_archive.html', context)
 
