893e07752b95f41acb2f70ce16a75c098136ba05
harrymusyoka <musyokaoracle@gmail.com>
2022-09-08 22:42:47 +0300

    upd01

--- apps03/urls.py
+++ apps03/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('apps03/<int:year>/', views.year_archive),
-    path('apps03/<int:year>/<int:month>/', views.month_archive),
-    path('apps03/<int:year>/<int:month>/<int:pk>/', views.article_detail),
+    path('apps03/year_archive/', views.year_archive),
+    path('apps03/login/', views.login),
+   
 ]
 
