7da15d2ead359872842b0ce815591664135fe134
harrymusyoka <musyokaoracle@gmail.com>
2022-09-10 16:46:48 +0300

    upd01

--- apps03/urls.py
+++ apps03/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path, url
-
+from django.urls import path 
 from . import views
 
 urlpatterns = [
     path('apps03/year_archive/', views.year_archive),
     path('apps03/login/', views.login),
     path('apps03/checkins_list/', views.checkins_list),
-   
+    path('apps03/checkinsadd/', views.checkinsadd),
     path('apps03/view_booking/', views.view_booking),
-    url(r'', checkinsadd_view, name='checkinsadd'),
+    
     
 ]
 
