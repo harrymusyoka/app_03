3dd79232586bbdfb5d2f0381dfff7aafed650ea4
harrymusyoka <musyokaoracle@gmail.com>
2022-09-10 16:16:03 +0300

    upd01

--- apps03/urls.py
+++ apps03/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('apps03/year_archive/', views.year_archive),
     path('apps03/login/', views.login),
     path('apps03/checkins_list/', views.checkins_list),
-    path('apps03/checkinsadd/', views.checkinsadd),
+    path('checkinsadd/', views.checkinsadd),
     path('apps03/view_booking/', views.view_booking),
     
     
