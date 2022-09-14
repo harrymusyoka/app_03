85f7cef7a1589741fc07439dc20414ee63c5af27
harrymusyoka <musyokaoracle@gmail.com>
2022-09-14 20:53:52 +0300

    upd01

--- app_03/settings.py
+++ app_03/settings.py
@@ -55,7 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps-03-3b3ea.ondigitalocean.app',
+    'apps03'
+ #   'apps-03-3b3ea.ondigitalocean.app',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
